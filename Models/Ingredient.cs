a7dec91efe36870c7fbc6b47d29d37183e5496b2
hudis flax <yf56229@gmail.com>
2022-09-19 16:50:29 +0300

    added ingredients class etc.

---
+++ Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿namespace theFoodCampus.Models
+{
+    /// <summary>
+    /// ingredient class with all relevant information
+    /// </summary>
+    public class Ingredient
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+
+        public MeasurementType MeasurementUnit { get; set; }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿namespace theFoodCampus.Models
+{
+    /// <summary>
+    /// recipe class consisting of id, name, tags , ingredients
+    /// </summary>
+    public class Recipe
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<int> Tags { get; set; }
+        public List<Ingredient> Ingredients{ get; set; }
+        public List<string> Instructions{ get; set; }
+        public string Image { get; set; }
+    }
+}
