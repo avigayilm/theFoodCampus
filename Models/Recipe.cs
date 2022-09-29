1d13d4ea7090dcb067cbe4819e2e83affa70e724
avigayilm <amandel@g.jct.ac.il>
2022-09-29 15:50:16 +0300

    Added the gateway connection to the Usda

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -11,7 +11,7 @@ namespace theFoodCampus.Models
     public class Recipe
     {
 
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
 
         [DisplayName("Descrption")]
--- Models/USDAAdapter.cs
+++ Models/USDAAdapter.cs
@@ -1,9 +1,21 @@
-﻿namespace theFoodCampus.Models
+﻿using RestSharp;
+
+namespace theFoodCampus.Models
 {
     public class USDAAdapter
     {
-        public string Check()
+        public string Check(string ingredients, string keyword)
         {
+            if (keyword == " ")
+                keyword = "-";
+            string Url = $"http://localhost:5202/api/usda?ingredients={ingredients}&keyword=d{keyword}";
+
+            var client = new RestClient(Url);
+
+            var request = new RestRequest(new Uri(Url), Method.Get);
+
+            RestResponse response = client.Execute(request);
+
             // here we have to connect to the gateway and
             //get information about the weather
             return $"Here we write the weather we got from gateway";
